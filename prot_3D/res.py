b9a321329b23d4d8995c37550f47c1be8144dd11
Yvan Sraka <yvan@sraka.pw>
2018-10-22 15:59:14 +0200

    Add types annotations in Atom, Prot and Res class declarations

--- prot_3D/res.py
+++ prot_3D/res.py
@@ -10,31 +10,30 @@ class Res:
 
     Prameters
     ---------
-    res_num : scalar value
+    res_num: scalar value
         Residue number as described in the PDB file.
 
-    res_name : string
+    res_name: string
         Residue name. Use 3-letters code.
 
-    atoms : dictionary of Atom instances
+    atoms: dictionary of Atom instances
         Atoms of the residue. The dictionary must be built as :
 
             atoms = {atom_name: atom, ...}
-                atom_name : string
+                atom_name: string
                     Atom name as described in the PDB file (For example : 'CA'
                     for carbon alpha).
-                atom : Atom instance
+                atom: Atom instance
                     Atom data structure.
     """
 
-    def __init__(self, res_num, res_name, atoms=dict()):
-
+    def __init__(self: Res, res_num: int, res_name: str, atoms=dict()) -> None:
         self.res_num = res_num
         self.res_name = res_name
         self.atoms = atoms
 
     # String conversion
-    def __str__(self):
+    def __str__(self: Res) -> str:
         """
         Define string conversion.
         Used by print to represent the object.
@@ -43,7 +42,7 @@ class Res:
                 self.res_name, self.res_num, self.atoms)
 
     # Iterability implementation
-    def __iter__(self):
+    def __iter__(self: Res):
         """
         Create an iterator.
         Iterate the atoms of the residue.
@@ -51,14 +50,14 @@ class Res:
         return iter(self.atoms.values())
 
     # Operator overload
-    def __getitem__(self, key):
+    def __getitem__(self: Res, key: int) -> Atom:
         """
         Overload the '[]' index operator.
         Access an atom by its atom_name.
         """
         return self.atoms[key]
 
-    def __setitem__(self, key, item):
+    def __setitem__(self: Res, key: int, item: Atom) -> None:
         """
         Overload of item assignment.
         Set an atom.
