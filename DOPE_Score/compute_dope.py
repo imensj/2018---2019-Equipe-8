476b107580b98cb5cd94bfd754d1698ba5dce61b
Antoine Gagelin <antoine.gagelin@gmail.com>
2018-11-05 17:00:19 +0100

    Create the DOPE dictionary row by row

--- DOPE_Score/compute_dope.py
+++ DOPE_Score/compute_dope.py
@@ -22,41 +22,19 @@ This is because it is necessary to iterate twice on atom_selection list (see DOP
     ==> find a more inteligent way to build the dictionary
 """
 
-from DOPE_Score.dope_to_dictionary import DOPE_to_dict  # pyre-ignore
-from DOPE_Score.score_per_chain import DOPE_score  # pyre-ignore
+from .score_per_chain import DOPE_score  # pyre-ignore
 import numpy as np  # pyre-ignore
 import pandas as pd  # pyre-ignore
 
 
-def compute_DOPE_score(threadings, path_to_dope_par, atom_selection="CA", mean_per_residue=True):
+def compute_DOPE_score(threadings, atom_selection=("CA"), mean_per_residue=True):
 
-    dope_matrix = pd.read_table(
-        path_to_dope_par, header=None, sep=" ")   # Read matrix dope.par
-    # Column 1 contains atoms
-    atoms = np.unique(dope_matrix.iloc[:, 1])
     # Reproduced behaviour when theadings is a list
     threadings = [threadings] if threadings.__class__ != list else threadings
-    if atom_selection:
-        atom_selection = [atom_selection] if atom_selection.__class__ != list else atom_selection
-        if all(atm in atoms for atm in atom_selection):
-            # Subsample dope_matrix to later on create a dictionary faster
-            dope_matrix = dope_matrix[dope_matrix.iloc[:, 1].isin(atom_selection)]
-            # Subsample dope_matrix to later on create a dictionary faster
-            dope_matrix = dope_matrix[dope_matrix.iloc[:, 3].isin(atom_selection)]
-        else:
-            raise ValueError("compute_DOPE_score: parameter 'atom_selection' contains erroneous values.")
-    else:
-        # No atom selection when atome_selection = None or atome_selection = False
-        atom_selection = atoms
-
-    dope_dict = DOPE_to_dict(dope_matrix=dope_matrix,
-                             atom_selection=atom_selection)
-
     scores = dict()
     for thread in threadings:
         for chain in thread:
-            score_thread = DOPE_score(Chain=thread[chain],
-                                      dope_dict=dope_dict,
+            score_thread = DOPE_score(chain=thread[chain],
                                       atom_selection=atom_selection,
                                       mean_per_residue=mean_per_residue)
             scores.update(score_thread)
--- DOPE_Score/score_per_chain.py
+++ DOPE_Score/score_per_chain.py
@@ -17,45 +17,44 @@ Returns a DOPE score for 1 Chain of 1 protein
 import numpy as np  # pyre-ignore
 import itertools
 from math import sqrt
+from .dope_dict import dope_dict
 
 
-def DOPE_score(Chain, dope_dict, atom_selection, mean_per_residue=True):
+def DOPE_score(chain, atom_selection=None, mean_per_residue=True):
 
-    Chain_name = Chain.chain_name
-    Prot_name = Chain.prot_name
-    Chain = Chain.residues
+    chain_name = chain.chain_name
+    prot_name = chain.prot_name
+    res = chain.residues
     print("#~#~#~# Computing DOPE score for %s (Chain '%s') #~#~#~#\n" %
-          (Prot_name, Chain_name))
+          (prot_name, chain_name))
     SCORE = 0
     # Tupple of all doubletons (Residue x, Residue y) for all x,y in res_number such that x != y
-    res_combi = itertools.combinations(Chain.keys(), 2)
+    res_combi = itertools.combinations(res.keys(), 2)
     for res1, res2 in res_combi:
-        if len(atom_selection) > 1:
-            list_atm1 = list(Chain[res1].atoms.keys())
-            list_atm1 = [atm for atm in list_atm1 if atm in atom_selection]
+        res_name1 = res[res1].res_name
+        res_name2 = res[res2].res_name
 
-            list_atm2 = list(Chain[res2].atoms.keys())
-            list_atm2 = [atm for atm in list_atm2 if atm in atom_selection]
-
-            # Tupple of all doubletons (Atom x, Atom y) for all (x,y) in (res1.atoms, res2.atom2) such that Atom x and y are in atom_selection
-            atm_combi = itertools.product(list_atm1, list_atm2)
-        # Maybe a little bit faster when len(atom_selection) = 1
+        if not atom_selection:
+            set_atm1 = set(res[res1].atoms.keys())
+            set_atm2 = set(res[res2].atoms.keys())
         else:
-            # Just to reproduce the structure of atm_combi when len(atom_selection) > 1
-            atm_combi = [(atom_selection[0], atom_selection[0])]
-
+            set_atm1 = set(res[res1].atoms.keys()) & set(atom_selection)
+            set_atm2 = set(res[res2].atoms.keys()) & set(atom_selection)
+            # Tupple of all doubletons (Atom x, Atom y) for all (x,y) in (res1.atoms, res2.atom2) such that Atom x and y are in atom_selection
+        atm_combi = itertools.product(set_atm1, set_atm2)
         mean = []
         for atm1, atm2 in atm_combi:
 
-            coord1 = Chain[res1].atoms[atm1].coord
-            coord2 = Chain[res2].atoms[atm2].coord
+            coord1 = res[res1].atoms[atm1].coord
+            coord2 = res[res2].atoms[atm2].coord
             distance = coord1 - coord2
             distance = [d**2 for d in distance]
             distance = sqrt(sum(distance))
             # Operations on distances : compute vector's norm
 
             if distance <= 15.0 and distance >= 0.25:
-                mean.append(dope_dict[Chain[res1].res_name][atm1][Chain[res2].res_name][atm2][round((distance - 0.25)//0.5)])
+                pot_idx = round((distance - 0.25)//0.5)
+                mean.append(dope_dict[res_name1][atm1][res_name2][atm2][pot_idx])
 
         # i.e. at least 1 distance in [0.25,0.15] because
         if len(mean) > 0:
